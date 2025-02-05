656d699a6dc1bac53310cdfbccbdef3381935793
매빈 <77741296+aoqlsdl@users.noreply.github.com>
2025-02-05 22:16:00 +0900

    [Gold IV] Title: 문자열 폭발, Time: 736 ms, Memory: 50432 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,22 @@
+# 9935. 문자열 폭발
+import os
+
+arr = list(input())
+bomb = input()
+len_bomb = len(bomb)
+
+result = []
+
+for a in arr:
+    result.append(a)
+
+    if len(result) < len_bomb:
+        continue
+
+    if ''.join(result[-len_bomb:]) == bomb:
+        del result[-len_bomb:]
+
+if len(result) == 0:
+    print("FRULA")
+else:
+    print(''.join(result))
