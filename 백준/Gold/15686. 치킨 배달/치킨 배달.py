55743d029e47cb68e8dee46901895993356109e5
매빈 <77741296+aoqlsdl@users.noreply.github.com>
2025-04-29 21:54:51 +0900

    [Gold V] Title: 치킨 배달, Time: 524 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 c = [list(map(int, input().split())) for _ in range(n)]
 
-# 치킨 거리 구하기 --> 도시의 치킨 거리의 최소값 구하기 가능
+# 문제의 목적: 치킨 거리 구하기 --> 도시의 치킨 거리의 최소값 구하기 가능
 # 치킨집/집 위치 저장
 chicken = []
 house = []
@@ -18,14 +18,13 @@ for i in range(n):
             house.append((i, j)) # 집 좌표 저장
         elif c[i][j] == 2:
             chicken.append((i, j)) # 치킨집 좌표 저장
-
-# 치킨집 m개를 뽑은 모든 경우의 수를 고려하기
+    
+# 치킨거리 구하기
 for chick in combinations(chicken, m):
     d = 0 # 거리 초기화
     
-    # 집을 하나씩 골라서, 각각의 치킨거리 도출
     for h in house:
-        h_d = sys.maxsize # 한 가정 당 치킨거리 초기화
+        h_d = sys.maxsize # 집에서 치킨집까지의 거리 초기화
         for j in range(m):
             h_d = min(h_d, abs(h[0] - chick[j][0]) + abs(h[1] - chick[j][1]))
         d += h_d
